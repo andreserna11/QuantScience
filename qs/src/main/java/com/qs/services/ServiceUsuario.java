3a76dea2690b301b120a6a516bf8ce93c8d48502
Juan Pablo Arboleda Tolosa <Juan Pablo Arboleda Tolosa>
2017-07-29 16:18:14 -0500

    se añade el servicio de usuario

--- qs/src/main/java/com/qs/services/ServiceUsuario.java
+++ qs/src/main/java/com/qs/services/ServiceUsuario.java
@@ -1,7 +1,10 @@
 package com.qs.services;
 
+import org.springframework.stereotype.Service;
+
 import com.qs.modelos.UsuariosQs;
 
+@Service
 public interface ServiceUsuario {
 	
 	public UsuariosQs getUsuarioService(String correo);
--- qs/src/main/java/com/qs/services/ServiceUsuarioImpl.java
+++ qs/src/main/java/com/qs/services/ServiceUsuarioImpl.java
@@ -1,12 +1,14 @@
 package com.qs.services;
 
+import org.springframework.stereotype.Service;
+
 import com.qs.modelos.UsuariosQs;
 
+@Service
 public class ServiceUsuarioImpl implements ServiceUsuario {
 
 	public UsuariosQs getUsuarioService(String correo) {
 		UsuariosQs user = new UsuariosQs();
-		
 		
 		
 		
