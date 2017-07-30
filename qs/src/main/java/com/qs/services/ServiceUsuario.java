01d2b51ffb6001c7d369a276e800ccb673afe395
Juan Pablo Arboleda Tolosa <Juan Pablo Arboleda Tolosa>
2017-07-29 19:30:02 -0500

    se añade registro del usuario

--- qs/src/main/java/com/qs/services/ServiceUsuario.java
+++ qs/src/main/java/com/qs/services/ServiceUsuario.java
@@ -1,12 +1,11 @@
 package com.qs.services;
 
-import org.springframework.stereotype.Service;
-
 import com.qs.modelos.qs.usuario;
 
-@Service
 public interface ServiceUsuario {
 	
 	public usuario getUsuarioService(String correo);
+	
+	public boolean insertUsuarioService(usuario user);
 	
 }
--- qs/src/main/java/com/qs/services/ServiceUsuarioImpl.java
+++ qs/src/main/java/com/qs/services/ServiceUsuarioImpl.java
@@ -1,19 +1,32 @@
 package com.qs.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.qs.dao.qs.usuarioMapper;
 import com.qs.modelos.qs.usuario;
+import com.qs.modelos.qs.usuarioExample;
 
 @Service
 public class ServiceUsuarioImpl implements ServiceUsuario {
 
+	@Autowired
+	usuarioMapper uMapper;
+	
 	public usuario getUsuarioService(String correo) {
-		usuario user = new usuario();
+		usuarioExample uExample = new usuarioExample();
+		uExample.or().andEmailEqualTo(correo);
 		
-		
-		
+		List<usuario> result = uMapper.selectByExample(uExample);
+		usuario user = result.get(0);
 		
 		return user;
+	}
+
+	public boolean insertUsuarioService(usuario user) {
+		return uMapper.insert(user) == 1;
 	}
 
 }
