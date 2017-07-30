de8cbfeaa3de89927600d4fd3ff5def03c52011a
Valentín Quintero Castrillón <vquintec@users.noreply.github.com>
2017-07-30 06:45:43 -0500

    Implementacion del modulo de contenido de entrenamientos

---
+++ qs/src/main/java/com/qs/services/ServiceMC.java
@@ -0,0 +1,9 @@
+package com.qs.services;
+import java.util.List;
+import com.qs.modelos.qs.membresia_contenido;
+
+public interface ServiceMC {
+	
+	public List<membresia_contenido> getMCService(int id);
+	
+}
---
+++ qs/src/main/java/com/qs/services/ServiceMembresia.java
@@ -0,0 +1,9 @@
+package com.qs.services;
+
+import com.qs.modelos.qs.membresia;
+
+public interface ServiceMembresia {
+	
+	public membresia getMembresiaService(int id);
+	
+}
